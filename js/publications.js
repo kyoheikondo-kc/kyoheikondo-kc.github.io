9d7e01715618cb1e898faffe2e969bb7543c7ad4
KyoheiKondo <53456480+kyoheikondo-kc@users.noreply.github.com>
2025-12-30 22:05:33 +0900

    update files

--- js/publications.js
+++ js/publications.js
@@ -4,7 +4,7 @@ const publicationsData = [
         "title": "The different strategies for good sleep in each chronotype",
         "journal": "Chronobiology International",
         "year": 2025,
-        "image": "https://www.tandfonline.com/cms/asset/3d144f69-62cd-48ee-a7a7-7cafb82d8993/icbi_a_2584196_f0003_oc.jpg",
+        "image": "assets/images/publications/chronotype_sleep.jpg",
         "tags": [
             "Sleep",
             "Chronobiology",
@@ -16,7 +16,7 @@ const publicationsData = [
         "title": "Persistence of adaptation and its association with sleep quality in the tropical climate",
         "journal": "Building and Environment",
         "year": 2025,
-        "image": "https://ars.els-cdn.com/content/image/1-s2.0-S0360132325005281-gr10.jpg",
+        "image": "assets/images/publications/tropical_adaptation.jpg",
         "tags": [
             "Sleep",
             "Thermal Comfort",
@@ -29,7 +29,7 @@ const publicationsData = [
         "title": "Transition of adaptation methods with the increase of income",
         "journal": "Sustainability",
         "year": 2025,
-        "image": "https://www.researchgate.net/publication/390031836/figure/fig2/AS:11431281337342109@1743427471067/The-proportion-of-thermal-adaptations-during-gathering-activities-for-each-apartment-type_W640.jpg",
+        "image": "assets/images/publications/adaptation_income.jpg",
         "tags": [
             "Thermal Comfort",
             "Adaptation",
@@ -42,7 +42,7 @@ const publicationsData = [
         "title": "Sitting behavior and comfortability",
         "journal": "IOP Conference Series: Earth and Environmental Science",
         "year": 2024,
-        "image": "https://www.researchgate.net/publication/380996165/figure/fig2/AS:11431281248226820@1717080873113/Cluster-Location_W640.jpg",
+        "image": "assets/images/publications/sitting_behavior.jpg",
         "tags": [
             "Behavior",
             "Thermal Comfort"
@@ -53,7 +53,7 @@ const publicationsData = [
         "title": "Regional difference of sleep patterns during pandemic",
         "journal": "Chronobiology International",
         "year": 2023,
-        "image": "https://www.tandfonline.com/cms/asset/b9635064-47f9-4e98-bd6f-f79c149f04be/icbi_a_2340719_f0001_oc.jpg",
+        "image": "assets/images/publications/pandemic_sleep.jpg",
         "tags": [
             "Sleep",
             "Chronobiology",
@@ -65,7 +65,7 @@ const publicationsData = [
         "title": "Effects of window opening on sleep quality during transitional season in Japan",
         "journal": "Indoor Air",
         "year": 2022,
-        "image": "https://www.researchgate.net/publication/375036893/figure/fig4/AS:11431281202072889@1698686119248/Setting-points-a-elevation-of-a-bed-during-sleep-and-b-plan-for-a-bed-during-sleep_W640.jpg",
+        "image": "assets/images/publications/window_opening.jpg",
         "tags": [
             "Sleep",
             "IAQ",
